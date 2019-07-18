59508be8f3b5edafdf8d0b514b75ea316dbc0d39
zimm <romsssa@gmail.com>
2019-07-18 21:45:06 +0300

    Implement game logic

--- src/Game.php
+++ src/Game.php
@@ -10,23 +10,91 @@ class Game
     protected $players = [];
 
     /**
-     * @var Rules
+     * @var GameField
      */
-    protected $rules;
+    protected $field;
+
+    /**
+     * @var string
+     */
+    protected $lastMarkPlayer;
 
     public function __construct()
     {
-        $this->rules = new Rules($this);
+        $this->field = new GameField();
     }
 
     public function addPlayer(string $name): void
     {
-        $this->rules->assertCanAddPlayer();
+        $this->assertCanAddPlayer($name);
         $this->players[] = $name;
     }
 
     public function getPlayers(): array
     {
         return $this->players;
+    }
+
+    public function getField(): GameField
+    {
+        return $this->field;
+    }
+
+    public function putMark($player, Point $coordinate): void
+    {
+        $this->assertCanPutMark($player, $coordinate);
+        $this->lastMarkPlayer = $player;
+
+        $this->field->putMark($coordinate, $this->getPlayerMark($player));
+    }
+
+    public function getPlayerMark($player)
+    {
+        return array_search($player, $this->players);
+    }
+
+    public function getWinner()
+    {
+        $mark = $this->field->getWinnerMark();
+
+        if ($mark === null) {
+            return null;
+        }
+
+        return $this->players[$mark];
+    }
+
+    protected function assertCanAddPlayer($playerName): void
+    {
+        if (count($this->players) >= 2) {
+            throw new \LogicException('Only 2 players can be added');
+        }
+
+        if ($this->getPlayerMark($playerName) !== false) {
+            throw new \LogicException('Player already exists');
+        }
+    }
+
+    protected function assertCanPutMark($playerName, Point $coordinate): void
+    {
+        if (count($this->players) !== 2) {
+            throw new \LogicException('Players should be registered first');
+        }
+
+        if ($this->getPlayerMark($playerName) === false) {
+            throw new \LogicException('Players does not exist');
+        }
+
+        if ($this->lastMarkPlayer === $playerName) {
+            throw new \LogicException('Player turn is wrong');
+        }
+
+        if ($this->field->getMark($coordinate) !== null) {
+            throw new \LogicException('Point with desired coordinates already marked');
+        }
+
+        if ($this->field->getWinnerMark() !== null) {
+            throw new \LogicException('Game is over');
+        }
     }
 }
---
+++ src/Point.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace TicTacToe;
+
+class Point
+{
+    /**
+     * @var int
+     */
+    protected $x;
+
+    /**
+     * @var int
+     */
+    protected $y;
+
+    public function __construct(int $x, int $y)
+    {
+        $this->assertCoordinates($x, $y);
+
+        $this->x = $x;
+        $this->y = $y;
+    }
+
+    public static function fromArray(array $coordinate)
+    {
+        return new static($coordinate[0], $coordinate[1]);
+    }
+
+    public function getX(): int
+    {
+        return $this->x;
+    }
+
+    public function getY(): int
+    {
+        return $this->y;
+    }
+
+    protected function assertCoordinates($x, $y)
+    {
+        foreach ([$x, $y] as $coordinate) {
+            if ($coordinate < 0 || $coordinate > 2) {
+                throw new \InvalidArgumentException('Invalid coordinate');
+            }
+        }
+    }
+}
